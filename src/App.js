c6bb265b0d9dd5a55c86b7d2dbd18a902bf3dbcf
Jake M <scudsterr@gmail.com>
2020-01-01 16:05:28 -0500

    organizing structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Jumbotron } from 'react-bootstrap';
+import SearchForm from './components/SearchForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Jumbotron>
+        <Container>
+          <h1>Weather App</h1>
+          <p>
+            I am building a weather app.
+          </p>
+        
+          <SearchForm />
+        </Container>
+      </Jumbotron>
     </div>
   );
 }
