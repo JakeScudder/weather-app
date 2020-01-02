278915e4c3040616e6c15ffdaacbebba5792ed29
Jake M <scudsterr@gmail.com>
2020-01-01 19:34:36 -0500

    Styling and Results Component

---
+++ src/components/Results.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Container,
+} from 'react-bootstrap';
+
+const Results = () => {
+  return (
+  <Container>
+    <h1> This is a placeholder for the results section</h1>
+  </Container>
+  )
+};
+
+export default Results;
