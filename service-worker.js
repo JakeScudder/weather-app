e6dc5b0ddac9815e774449ea9d0708e7aa966e9e
Jake M <scudsterr@gmail.com>
2020-03-02 18:33:56 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.5c33d141812e7fde175b3135d7d46033.js"
+  "/weather-app/precache-manifest.c6468bb6c7d7ef8ed2a1c78b27997d34.js"
 );
 
 self.addEventListener('message', (event) => {
